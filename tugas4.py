3827d066e1480aff4a2eaa9290f7700b4dbd2469
masee24 <mas7aji24@gmail.com>
2022-10-15 19:47:33 +0700

    first commit

---
+++ tugas4.py
@@ -0,0 +1,42 @@
+#ctype_data_int
+pertama = 44
+print(pertama)
+print(type(pertama))
+
+#type_data_float
+kedua = 9.7
+print(kedua)
+print(type(kedua))
+
+#type_data_tuple
+ketiga = 5, 4
+print(ketiga)
+print(type(ketiga))
+
+#type_data_String
+keempat = "ayo berangkat"
+print(keempat)
+print(type(keempat))
+
+#type_data_boolean
+angka1 = 24
+angka2 = 19
+penjumlahan = angka1 + angka2
+pengurangan  = angka1 - angka2
+print(penjumlahan < pengurangan)
+print(type(penjumlahan < pengurangan))
+
+#type_data_complex
+B2 = complex(4, 5)
+print(B2)
+print(type(B2))
+
+#type_data_list
+Al = ([1,2,5,6])
+print(Al)
+print(type(Al))
+
+#type_data_dictionary
+Ping = {"nama":"Aji", 'kelas': 12}
+print(Ping)
+print(type(Ping))
